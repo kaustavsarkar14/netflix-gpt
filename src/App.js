e545252625a1016bd7f4dd00fc7976ac6cdac453
kaustavsarkar14 <sarkar.kaustav14@gmail.com>
2023-12-22 19:16:33 +0530

    redux toolkit setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { Provider } from "react-redux";
 import Body from "./components/Body";
+import store from "./redux/store";
 
 function App() {
   return (
-    <Body/>
+    <Provider store={store} >
+      <Body />
+    </Provider>
   );
 }
 
---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "./userSlice";
+const store = configureStore({
+  reducer: { user: userReducer },
+});
+
+export default store;
+ 
