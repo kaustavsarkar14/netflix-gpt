06dc1c5443b418230118e50ffe71544386bb3eb8
kaustavsarkar14 <sarkar.kaustav14@gmail.com>
2023-12-22 20:02:06 +0530

    github workflow delete

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,24 +1,38 @@
-import React from 'react'
-import Login from './Login'
-import Browse from './Browse'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import React, { useEffect } from "react";
+import Login from "./Login";
+import Browse from "./Browse";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { auth } from "../utils/firebase";
+import { useDispatch } from "react-redux";
+import { addUser } from "../redux/userSlice";
+import { onAuthStateChanged } from "firebase/auth";
 
 const Body = () => {
-    const appRouter = createBrowserRouter([
-        {
-            path: "/",
-            element : <Login/>
-        },
-        {
-            path: "/browse",
-            element : <Browse/>
-        },
-    ])
+  const dispatch = useDispatch();
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login />,
+    },
+    {
+      path: "/browse",
+      element: <Browse />,
+    },
+  ]);
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid, email, displayName } = user;
+        dispatch(addUser({ uid, email, displayName }));
+      } else {
+      }
+    });
+  }, []);
   return (
     <div>
-        <RouterProvider router={appRouter} />
+      <RouterProvider router={appRouter} />
     </div>
-  )
-}
+  );
+};
 
-export default Body
+export default Body;
