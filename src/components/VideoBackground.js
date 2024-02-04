c02143ef29916dbb2c508d5f743b8403d7d3760f
kaustavsarkar14 <sarkar.kaustav14@gmail.com>
2024-02-04 23:21:22 +0530

    placeholder video added for no trailer video

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -6,12 +6,13 @@ const VideoBackground = ({ id }) => {
   useMovieTrailer(id);
   const trailerVideo = useSelector((state) => state.movies.trailerVideo);
 
-  if (!trailerVideo) return;
   return (
-    <div className="w-full h-[50vh] md:h-screen overflow-hidden" >
+    <div className="w-full min-h-[50vh] h-[50vh] md:h-screen overflow-hidden">
       <iframe
-      className="md:w-full w-[190%] aspect-video md:h-auto"
-        src={`https://www.youtube.com/embed/${trailerVideo.key}?autoplay=1&mute=1&controls=0&loop=1`}
+        className="md:w-full w-[190%] aspect-video md:h-auto"
+        src={`https://www.youtube.com/embed/${
+          trailerVideo ? trailerVideo.key : "u31qwQUeGuM"
+        }?autoplay=1&mute=1&controls=0&loop=1`}
         title="Aquaman and the Lost Kingdom | Mouthful of Water Challenge"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       ></iframe>
